36b837c7c129a284394b34f95c49c8a587759a8b
SebProgJu <urregoarr00@gmail.com>
2022-02-17 22:15:19 -0500

    5.5 exercise

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,16 +1,19 @@
 import Blog from './Blog'
 import CreateBlog from './CreateBlog'
+import Toggleable from './Toggleable'
 
 const Blogs = ({ user, handleLogOut, blogs, setBlogs, setNotification }) => {
   return (
     <div>
       <h2>blogs</h2>
       <p>{user.name} logged in <button onClick={handleLogOut}>Logout</button></p>
-      <CreateBlog 
-        blogs={blogs}
-        setBlogs={setBlogs}
-        setNotification={setNotification}
-      />
+      <Toggleable>
+        <CreateBlog
+          blogs={blogs}
+          setBlogs={setBlogs}
+          setNotification={setNotification}
+        />
+      </Toggleable>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
---
+++ src/components/Toggleable.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const Toggleable = (props) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={() => setVisible(true)}>New Blogs</button>
+      </div>
+      <div style={showWhenVisible}>
+        {props.children}
+        <button onClick={() => setVisible(false)}>Cancel</button>
+      </div>
+    </div>
+  )
+}
+
+export default Toggleable
